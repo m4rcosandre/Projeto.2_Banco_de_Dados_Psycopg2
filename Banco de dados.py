3ea16a2d8bb22a7a6b723bbdeaadc01f8c048474
Marcos André O dos Santos <m4rcos.oliveira19@gmail.com>
2025-06-17 21:41:38 -0300

    Update Banco de dados.py

--- Banco de dados.py
+++ Banco de dados.py
@@ -1,51 +1,77 @@
 #Biblioteca padrão
 import psycopg2
 
-#conectando ao banco padrão
-try:
-  conn = psycopg2.connect(
-
-    dbname = "postgres",
-    user = "postgres",
-    password = "dedeuogostoso123",
-    host = "localhost",
-    port = "5432"
-  )
-  conn.autocommit = True
-  cur = conn.cursor()
-except:
-  print("Erro ao se conectar ao banco.")
-  
-def criar_banco(nome_database):
+ 
+# Conectar ao banco padrão 'postgres'
+def conectar_banco(nome_banco):
   try:
-    cur.execute(F"CREATE DATABASE{nome_database}")
-
-    print("Banco de dados criado com sucesso!")
-    cur.close()
-    conn.close()
-  except Exception as e:
-    print(f"Erro ao criar um banco de dados!: {e}")
-  
-
-
-
-
-
-def conectar(nome_banco_novo):
-
-  try:
-    conn = psycopg2.connect(
-
-      dbname = nome_banco_novo,
+    conexao = psycopg2.connect(
+      dbname = nome_banco,
       user = "postgres",
       password = "dedeuogostoso123",
       host = "localhost",
       port = "5432"
     )
+    print(f"Conectado ao banco {nome_banco} com sucesso!")
+    return conexao
+  except Exception as e:
+    print(f"Erro ao conectar ao banco. {e}")
+    return None
+  
 
-    cur = conn.cursor()
-    print(f"Banco de dados {nome_banco_novo} conectado!")
-    return conn,cur
-  except psycopg2.Error as n:
-    print(f'Erro ao se conectar ao banco {n}')
-    return None, None
+def criar_tabela(conexao):
+  cursor = conexao.cursor()
+  cursor.execute("""CREATE TABLE IF NOT EXISTS pessoas(
+    id SERIAL PRIMARY KEY,
+    nome TEXT NOT NULL,
+    email TEXT NOT NULL,                   
+    contato NUMERIC             
+      )
+  """)
+  conexao.commit()
+  cursor.close()
+  
+def inserir_valores(conexao):
+  nome = input('Nome: ')
+  email = input('email: ')
+  contato = input('contato: ')
+  cursor = conexao.cursor()
+  cursor.execute("""INSERT INTO pessoas (nome, email, contato) 
+                 VALUES (%s, %s, %s)""", (nome, email, contato)) 
+  conexao.commit()
+  cursor.close()
+
+def deletar_valor(conexao):
+  id_alvo = input('Digite o ID da pessoa que deseja deletar: ')
+  cursor = conexao.cursor()
+  cursor.execute("""DELETE FROM pessoas WHERE id = %s""", (id_alvo,))
+
+  conexao.commit()
+  cursor.close()
+  print("registro deletado com sucesso.")
+
+
+def atualizar_valor(conexao):
+  id_alvo = input("Diga aqui o ID da pessoa que deseja atualizar os dados")
+  nome = input("Novo nome: ")
+  email = input("Novo email: ")
+  contato = input("Novo contato: ")
+
+  cursor = conexao.cursor()
+  cursor.execute("""UPDATE pessoas 
+                 SET nome = %s, email = %s, contato = %s 
+                 WHERE id = %s""", (nome,email,contato,id_alvo))
+  conexao.commit()
+  cursor.close()
+  print("Registro atualizado com sucesso.")
+
+
+
+
+nome_banco = input("Digite o nome do banco de dados existente para imprimir os dados: ")
+conexao = conectar_banco(nome_banco)
+
+criar_tabela(conexao)
+inserir_valores(conexao)
+atualizar_valor(conexao)
+deletar_valor(conexao)
